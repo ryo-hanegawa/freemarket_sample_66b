21783049ef7c01681ef2ecfb3c1591f92ce3b8a3
O-yeah1980 <1980itikyuppa@gmail.com>
2020-03-16 23:44:37 +0900

    introduction basic

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,10 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server '52.193.147.222', user: 'ec2-user', roles: %w{app db web}
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
@@ -59,5 +61,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-server '52.193.147.222', user: 'ec2-user', roles: %w{app db web}
