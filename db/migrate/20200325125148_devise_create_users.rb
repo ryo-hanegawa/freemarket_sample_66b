25d8f6ee933e98e5e8fb3439545bedf38532fd49
O-yeah1980 <1980itikyuppa@gmail.com>
2020-03-25 22:19:21 +0900

    devise migrate

--- db/migrate/20200325125148_devise_create_users.rb
+++ db/migrate/20200325125148_devise_create_users.rb
@@ -4,8 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,             null: false
+      t.string :last_name,            null: false
+      t.string :first_name,           null: false
+      t.string :last_name_reading,    null: false
+      t.string :first_name_reading,   null: false
+      t.integer :birth_year,          null: false, default: 0
+      t.integer :birth_month,         null: false, default: 0
+      t.integer :birth_day,           null: false, default: 0
+      t.integer :phone_number,        null: false
+      t.string :email,                null: false, default: ""
+      t.string :encrypted_password,   null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +45,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname, unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
