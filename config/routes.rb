1a6cf6064e139efcb8fba19030af50e8eadc93da
ryo-hanegawa <snake0515hane@gmail.com>
2020-04-04 19:10:25 +0900

    軽微なビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
   resources :items, except: :index
-  get 'purchases/show'
+  resources :purchases,only: [:show]
   root "purchases#show"
 end
