bc6c45911c6823a087132879157c5e79ba8692b6
ryo-hanegawa <snake0515hane@gmail.com>
2020-04-15 22:31:00 +0900

    ビュー崩れ修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :items, except: :index
-  resources :users, only: :show do
-    resources :creditcards, only: [:index, :new, :create, :destroy]
-  end
+  resources :users, only: [:index, :show]
+  resources :creditcards, only: [:index, :new, :create, :destroy]
   resources :logouts, only: :index
   resources :exhibitions, only: [:index, :create]
   resources :purchases,only: [:show]
