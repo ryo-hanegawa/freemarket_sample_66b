714ed426c84971ba3892847bbf939e21d5baed54
too-03 <o3.426.61_family@i.softbank.jp>
2020-04-25 16:02:18 +0900

    ブランチ最新化

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :items, except: :index
   resources :exhibitions, only: [:index, :create]
   resources :purchases,only: [:show]
-  root "purchases#show"
 end
