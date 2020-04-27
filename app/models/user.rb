fb79599ec1a5b46813fa8942868dc2294b2bd7e1
O-yeah1980 <1980itikyuppa@gmail.com>
2020-04-27 11:47:46 +0900

    corrected migration files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
        has_many :sns_credentials, dependent: :destroy
+       has_one :address, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
