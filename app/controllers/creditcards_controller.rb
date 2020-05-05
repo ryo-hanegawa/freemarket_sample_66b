0aad9ac482ee4a5d94ee611ec774c2ee4a1ef92c
O-yeah1980 <1980itikyuppa@gmail.com>
2020-05-05 11:39:36 +0900

    payjp show action

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -28,6 +28,17 @@ class CreditcardsController < ApplicationController
     end
   end
 
+  def show
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.apl_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
   # def destroy
   # end
 end
