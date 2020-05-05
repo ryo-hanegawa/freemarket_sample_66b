8e8900367b9359046e81892553421c3b79c87218
too-03 <o3.426.61_family@i.softbank.jp>
2020-05-06 00:26:47 +0900

    add_top_view_product_list

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
-  def index
+  def index # viewファイルに持っていくインスタンス変数を作成
     # N+1問題を解消し、itemsテーブルの指定のレコードを@itemsに代入
-    @items = Item.select(:id, :name, :price).includes(:images).order(:id[ASC])
+    @items = Item.select(:id, :name, :price).includes(:images).order("id DESC")
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_122054) do
+ActiveRecord::Schema.define(version: 2020_04_27_075214) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -57,11 +57,6 @@ ActiveRecord::Schema.define(version: 2020_04_28_122054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "product_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
