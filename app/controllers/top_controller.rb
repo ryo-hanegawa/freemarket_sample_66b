cb621bd6b756117a55066f5e098ae4d5004c1fae
too-03 <o3.426.61_family@i.softbank.jp>
2020-05-07 01:48:06 +0900

    未出品時の表示を追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   def index # viewファイルに持っていくインスタンス変数を作成
+    # itemsテーブルのレコード数を@numに代入
+    @num = Item.count
     # N+1問題を解消し、itemsテーブルの指定のレコードを@itemsに代入
     @items = Item.select(:id, :name, :price, :buyer).includes(:images).order("id DESC")
   end
