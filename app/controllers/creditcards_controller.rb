11f908721814ae41d201f8de8b5fbf0b891fccd6
O-yeah1980 <1980itikyuppa@gmail.com>
2020-05-07 03:33:16 +0900

    corrected mypage view

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -7,7 +7,7 @@ class CreditcardsController < ApplicationController
 
   def new
     card = Creditcard.where(user_id: current_user.id)
-    redirect_to action "show" if card.exists?
+    redirect_to action: "show" if card.exists?
   end
 
   def pay
