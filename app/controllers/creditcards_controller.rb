eb5f6ef5dee585315d502cf1968ab2b2c74ad2fb
O-yeah1980 <1980itikyuppa@gmail.com>
2020-05-07 16:44:02 +0900

    create set_card method

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,10 +1,9 @@
 class CreditcardsController < ApplicationController
-  
   require "payjp"
+  before_action :set_card, only: [:new, :show, :delete]
 
   def new
-    card = Creditcard.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    redirect_to action: "show" if set_card.exists?
   end
 
   def pay
@@ -25,26 +24,30 @@ class CreditcardsController < ApplicationController
   end
 
   def show
-    card = Creditcard.where(user_id: current_user.id).first
-    if card.blank?
+    if set_card.blank?
       redirect_to action: "new"
     else
       Payjp.api_key = Rails.application.credentials[:payjp_private_key]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(set_card.first.customer_id)
+      @default_card_information = customer.cards.retrieve(set_card.first.card_id)
       @exp_month = @default_card_information.exp_month.to_s
       @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
     end
   end
 
   def delete
-    card = Creditcard.where(user_id: current_user.id).first
-    if card.present?
+    if set_card.present?
       Payjp.api_key = Rails.application.credentials[:payjp_private_key]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(set_card.first.customer_id)
       customer.delete
-      card.delete
+      set_card.first.delete
     end
     redirect_to action: "new"
   end
+
+  private
+  def set_card
+    Creditcard.where(user_id: current_user.id)
+  end
+
 end
