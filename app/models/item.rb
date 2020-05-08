98329bd190d6e867ad51bc70e9cc86e638e221e1
too-03 <o3.426.61_family@i.softbank.jp>
2020-05-08 14:08:02 +0900

    ancestry準備完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :product, optional: true
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   # name(商品名) description(商品説明) category(カテゴリー) condition(商品の状態) prefecture(発送元の地域(都道府県)) deliberydate(発送までの日数) postage(配送料) price(価格)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_012734) do
+ActiveRecord::Schema.define(version: 2020_05_08_044907) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 2020_05_05_012734) do
     t.string "first_name_reading", null: false
     t.string "last_name_reading", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "creditcards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
