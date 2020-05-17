fcfb7ac3f0ab8193f0bca45bade1147a7f5e076b
O-yeah1980 <1980itikyuppa@gmail.com>
2020-05-17 17:41:07 +0900

    corrected prefecture

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user, optional: true
-  # belongs_to :product, optional: true
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
