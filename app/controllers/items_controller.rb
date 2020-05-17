732e749f1f55f3233d871270f69c32dc7bb88167
O-yeah1980 <1980itikyuppa@gmail.com>
2020-05-17 17:03:39 +0900

    item & product controller unification

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index, :new, :create, :edit, :update, :destroy]
   before_action :set_items, only: [:show, :edit, :update, :destroy]
-  before_action :item_update_params, only: :update
 
   def index
-    @items = Item.includes(:images)
   end
 
   def new
@@ -39,28 +37,29 @@ class ItemsController < ApplicationController
   def edit
     @item.images.cache_key unless @item.images.blank?
     @parents = Category.where(ancestry: nil)
-    @grandchild_category = @item.category
-    @child_category = @item.category.parent
     @parent_category = @item.category.root
+    @child = @parent_category.children
+    @child_category = @item.category.parent
+    @grandchild = @child_category.children
+    @grandchild_category = @item.category
   end
 
   def update
-    if params[:item][:images_attributes] == nil && @item.update(item_update_params)
-      redirect_to controller: :products, action: 'show'
-    else
-      @item.images.destroy
-      if @item.update(item_update_params)
-        redirect_to controller: :products, action: 'show'
+    @item.update(item_params)
+    if @item.save
+      redirect_to action: 'show'
       else
-        @images == nil
-        redirect_to(edit_product_path, notice: '編集できませんでした')
-      end
+        @items == nil
+        redirect_to(edit_item_path, notice: '編集できませんでした')
     end
   end
 
   def destroy
     @item.destroy if @item.user_id == current_user.id
-    redirect_to controller: :products, action: :index if @item.user_id == current_user.id && @item.destroy
+    redirect_to controller: :items, action: :delete_done if @item.user_id == current_user.id && @item.destroy
+  end
+
+  def delete_done
   end
 
   def search
@@ -85,14 +84,94 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :size, :category_id, :condition, :brand, :postage, :prefecture, :deliberydate, :price, :buyer, images_attributes: [:image]).merge(user_id: current_user.id)
-  end
-
-  def item_update_params
     params.require(:item).permit(:name, :description, :size, :category_id, :condition, :brand, :postage, :prefecture, :deliberydate, :price, :buyer, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
   end
+
+  # def item_update_params
+  #   params.require(:item).permit(:name, :description, :size, :category_id, :condition, :brand, :postage, :prefecture, :deliberydate, :price, :buyer, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+  # end
 
   def set_items
     @item = Item.find(params[:id])
   end
+  
+
+
+
+# before_action :authenticate_user!, only:[:edit, :update, :destroy]
+# before_action :set_item, only: [:edit, :show, :update, :destroy]
+
+  # def index
+  # end
+
+  # def edit
+  #   @item.images.cache_key unless @item.images.blank?
+
+  #   @grandchild_category = @item.category
+  #   @child_category = @item.category.parent
+  #   @parent_category = @item.category.root
+
+  #   @parents = Category.where(ancestry: nil)
+  #   @child = @parent_category.children
+  #   @grandchild = @child_category.children
+
+  # end
+
+  # def show
+  #   @images = @item.images
+  #   @image = @images.first
+    
+  #   @grandchild_category = @item.category
+  #   @child_category = @item.category.parent
+  #   @parent_category = @item.category.root
+
+  #   if @image == nil
+  #     redirect_to action: 'index'
+  #   end
+  # end
+
+  # def update
+  #   @item.update(item_params)
+  #   if @item.save
+  #     redirect_to action: 'show'
+  #     else
+  #       @items == nil
+  #       redirect_to(edit_item_path, notice: '編集できませんでした')
+  #   end
+  # end
+
+  # def destroy
+  #   @item.destroy if @item.user_id == current_user.id
+  #   redirect_to controller: :products, action: :index if @item.user_id == current_user.id && @item.destroy
+  # end
+
+  # def search_edit
+  #   respond_to do |format|
+  #     format.html
+  #     format.json do
+  #       # 親ボックスのidから子ボックスのidの配列を作成してインスタンス変数で定義
+  #       @children = Category.find(params[:parent_id]).children
+  #     end
+  #   end
+  # end
+
+  # def grandchildren_edit
+  #   respond_to do |format|
+  #     format.html
+  #     format.json do
+  #       # 子ボックスのidから孫ボックスのidの配列を作成してインスタンス変数で定義
+  #       @grandchildren = Category.find(params[:child_id]).children
+  #     end
+  #   end
+  # end
+
+  # private
+  # def item_update_params
+  #   params.require(:item).permit(:name, :description, :size, :category_id, :condition, :brand, :postage, :prefecture, :deliberydate, :price, :buyer, images_attributes: [:image]).merge(user_id: current_user.id)
+  # end
+
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
+
 end
