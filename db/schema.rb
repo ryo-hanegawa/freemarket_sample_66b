fa2d0f05e7f78fe909a2c9aeba15293d28e3b827
O-yeah1980 <1980itikyuppa@gmail.com>
2020-05-18 23:35:02 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_053410) do
+ActiveRecord::Schema.define(version: 2020_05_08_103442) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
